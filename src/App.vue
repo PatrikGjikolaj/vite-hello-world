6487ce31c39eeeb7efb25c90b85091aa3e83a241
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-18 16:53:10 +0200

    Esercizio completo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<script>
+//Importiamo i componenti da usare
+import AppTitle from './components/AppTitle.vue'
+
+//Definiamo l'oggetto che conterrà la lista dei componenti importati, che vogliamo usare
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<!-- Qui inseriamo l'HTML della nostra App -->
+<template>
+  <header>
+    <!-- Usiamo i componenti importati sopra -->
+    <!-- il contenuto è nei relativi file (cartella "components") -->
+    <AppTitle />
+  </header>
+
+  <main>
+
+  </main>
+  
+  <footer>
+
+  </footer>
+</template>
+
+<!-- Qui abbiamo eventuale stile del componente "App", che è il contenitore generale -->
+<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+//Definiamo il nome del componente ed eventualmente i componenti che usa (nessuno)
+export default {
+    name: "AppTitle",
+}
+</script>
+
+<!-- Quando si importa questo componente verrà visualizzato questo HTML al suo posto -->
+<template>
+    <h1 class="green">La mia prima app con <span class="yellow">Vite!</span></h1>
+    <p>Hello world</p>
+</template>
+
+<!-- Lo stile da applicare in QUESTO componente è il seguente -->
+<!-- In style.css abbiamo invece lo stile generico -->
+<style scoped>
+h1>span.yellow {
+    color: red;
+    text-decoration: underline;
+}
+</style>
