7a12353ca763308b300bad291c197f9992ee8de7
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-18 17:37:14 +0200

    aggiunto bootstrap

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -7,15 +7,13 @@ export default {
 
 <!-- Quando si importa questo componente verrà visualizzato questo HTML al suo posto -->
 <template>
-    <h1 class="green">La mia prima app con <span class="yellow">Vite!</span></h1>
-    <p>Hello world</p>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-4 offset-4 text-center">
+                <span>Ciao mondo</span>
+                <h1 class="my-3">La mia prima app con <span class="text-bg-primary">Vite!</span></h1>
+                <p>Hello world</p>
+            </div>
+        </div>
+    </div>
 </template>
-
-<!-- Lo stile da applicare in QUESTO componente è il seguente -->
-<!-- In style.css abbiamo invece lo stile generico -->
-<style scoped>
-h1>span.yellow {
-    color: red;
-    text-decoration: underline;
-}
-</style>
